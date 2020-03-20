760be4dfe09bf37c43fd6ad740caee8d3f99af1b
unknown <eng.ramybadr66@gmail.com>
2020-03-20 17:28:57 +0200

    clean dist

--- dist/common/constants/role-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var RoleType;
-(function (RoleType) {
-    RoleType["User"] = "USER";
-    RoleType["Admin"] = "ADMIN";
-    RoleType["Teacher"] = "TEACHER";
-})(RoleType = exports.RoleType || (exports.RoleType = {}));
-//# sourceMappingURL=role-type.js.map
--- dist/guards/auth.guard.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const passport_1 = require("@nestjs/passport");
-exports.AuthGuard = passport_1.AuthGuard('jwt');
-//# sourceMappingURL=auth.guard.js.map
