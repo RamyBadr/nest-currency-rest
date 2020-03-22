e2e16f241b5bae46d6af699380eae88028157190
unknown <ramybadr@3ddx.com>
2020-03-23 01:11:07 +0200

    clean app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,34 +10,9 @@ import { UserModule } from './modules/user/user.module';
 import { ConfigService } from './shared/services/config.service';
 import { SharedModule } from './shared/shared.module';
 import { FixerModule } from './modules/fixer/fixer.module';
-import { FixerGraphModule } from './modules/fixerGraph/fixer.graph.module';
-import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-    imports: [
-        SharedModule,
-        FixerModule,
-        FixerGraphModule,
-        // GraphQLModule.forRootAsync({
-        //     useFactory: () => ({
-        //         autoSchemaFile: 'schema.gql',
-        //         include: [FixerGraphModule],
-        //     }),
-        // }),
-        GraphQLModule.forRoot({
-            autoSchemaFile: 'schema.gql',
-        }),
-
-        // MathModule,
-        // AuthModule,
-        // UserModule,
-        // TypeOrmModule.forRootAsync({
-        //     imports: [SharedModule],
-        //     useFactory: (configService: ConfigService) =>
-        //         configService.typeOrmConfig,
-        //     inject: [ConfigService],
-        // }),
-    ],
+    imports: [SharedModule, FixerModule],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
